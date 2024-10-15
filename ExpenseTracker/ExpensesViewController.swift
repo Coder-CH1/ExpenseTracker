94c768b3406fbaca7bb0a5a3fcff674ec6107cdc
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-10-15 16:39:02 +0100

    added

---
+++ ExpenseTracker/ExpensesViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ExpenseViewController.swift
+//  ExpenseTracker
+//
+//  Created by Mac on 15/10/2024.
+//
+
+import UIKit
+
+class ExpensesViewController: UIViewController {
+    
+    lazy var expensesTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        //tableView.backgroundColor = .lightGray
+        tableView.register(ExpensesTableViewCell.self, forCellReuseIdentifier: "ExpensesTableViewCell")
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Subviews and Layout -
+    func setSubviewsAndLayout() {
+        view.addSubview(expensesTableView)
+        NSLayoutConstraint.activate([
+            expensesTableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            expensesTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 14),
+            expensesTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -14),
+            expensesTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20),
+        ])
+    }
+    
+}
+
+extension ExpensesViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 9
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ExpensesTableViewCell", for: indexPath) as! ExpensesTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        expensesTableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+}
---
+++ ExpenseTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExpenseTracker
+//
+//  Created by Mac on 15/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
